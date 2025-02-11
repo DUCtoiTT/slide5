d25195ba0ee45a3167549141e81a83b0383dd3da
DUCtoiTT <minhduc191104@gmail.com>
2025-02-11 10:49:17 +0700

    init project

--- App.js
+++ App.js
@@ -1,11 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from "react";
+import { View, Text, TouchableOpacity, StyleSheet, Alert } from "react-native";
+
+// Component nút bấm nhận props từ cha
+const ColorButton = ({ color, onPress }) => {
+  return (
+    <TouchableOpacity
+      style={[styles.button, { backgroundColor: color }]}
+      onPress={() => {
+        onPress(color); // Gửi màu lên component cha
+      }}
+    >
+      <Text style={[styles.buttonText, color === "yellow" && { color: "black" }]}>
+        {color.toUpperCase()}
+      </Text>
+    </TouchableOpacity>
+  );
+};
 
 export default function App() {
+  const [bgColor, setBgColor] = useState("white"); // State lưu màu nền của component cha
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={[styles.container, { backgroundColor: bgColor }]}>
+      
+      {["green", "blue", "brown", "yellow", "red", "black"].map((color) => (
+        <ColorButton key={color} color={color} onPress={setBgColor} />
+      ))}
     </View>
   );
 }
@@ -13,8 +33,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "center",
+    alignItems: "center",
+    padding: 10,
+  },
+
+  button: {
+    width: 200,
+    padding: 15,
+    margin:50,
+    marginVertical: 30,
+    alignItems: "center",
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: "white",
+    fontWeight: "bold",
   },
 });
